924b47d55a6573d09541c66887613f925d82b52d
Angelo_Nicoli (Gengio) <angelo.nicoli92@gmail.com>
2024-02-12 22:49:36 +0100

    Update Simple_Calc.py

--- Simple_Calc.py
+++ Simple_Calc.py
@@ -1,6 +1,7 @@
-#   Angelo NICOLI 2024
-#   LICENSE MIT 
-
+######################################################
+##                  Angelo NICOLI 2024              ##
+##                    LICENSE MIT                   ##
+######################################################
 
 from tkinter import *
 
@@ -8,7 +9,7 @@ from tkinter import *
 
 root = Tk()
 root.title("Simple Calculator")
-root.geometry("300x550")
+root.geometry("400x550")
 root.configure(bg='#252526')
 
 # Crea la barra per gli input dei dati
@@ -25,6 +26,29 @@ Operation_text = [""]
 print(Term_List["First_Number"])
 
 # e.insert(0, "Enter Your Name: ")
+
+
+button100 =[0]
+        
+def SciCalculator_Gui():
+    button_100 =  Button(root, text="1", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(1))
+    button_100.grid(row=2,column=40)
+    button100[0]= button_100
+
+
+def change_gui():
+    print("change")
+    SciCalculator_Gui()
+    root.geometry("500x550")
+
+def return_gui():
+    button100[0].destroy()
+    root.geometry("300x550")
+
+
+root.bind("b", return_gui)
+
+root.bind("a", change_gui)
 
 
 def res1():
@@ -59,10 +83,10 @@ filemenu.add_command(label="Exit", command=root.quit)
 
 # Second Menu column
 editmenu = Menu(menubar,tearoff=0)
-menubar.add_cascade(label="Edit", menu=editmenu)
+menubar.add_cascade(label="Mode", menu=editmenu)
 
-editmenu.add_command(label="01")
-editmenu.add_command(label="02")
+editmenu.add_command(label="Standard", command= return_gui )
+editmenu.add_command(label="Scientific", command= change_gui)
 
 # Third Menu column
 helpmenu = Menu(menubar, tearoff=0)
@@ -128,16 +152,16 @@ def button_equals():
 # Se non usiamo lambda, non possiamo usare le parentesi quando chiamiamo la funzione() 
 
 def Calculator_Gui():
-    button_1 =  Button(root, text="1", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(1))
-    button_2 =  Button(root, text="2", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(2))
-    button_3 =  Button(root, text="3", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(3))
-    button_4 =  Button(root, text="4", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(4))
-    button_5 =  Button(root, text="5", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(5))
-    button_6 =  Button(root, text="6", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(6))
-    button_7 =  Button(root, text="7", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(7))
-    button_8 =  Button(root, text="8", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(8))
-    button_9 =  Button(root, text="9", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(9))
-    button_0 =  Button(root, text="0", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(0))
+    button_1 =  Button(root, text="1",pady=20 , bg="#3e3e42",fg='white', command=lambda: button_click(1))
+    button_2 =  Button(root, text="2", pady=20, bg="#3e3e42",fg='white', command=lambda: button_click(2))
+    button_3 =  Button(root, text="3", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(3))
+    button_4 =  Button(root, text="4", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(4))
+    button_5 =  Button(root, text="5", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(5))
+    button_6 =  Button(root, text="6", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(6))
+    button_7 =  Button(root, text="7", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(7))
+    button_8 =  Button(root, text="8", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(8))
+    button_9 =  Button(root, text="9", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(9))
+    button_0 =  Button(root, text="0", pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(0))
 
     button_Par_OPEN =  Button(root, text="(", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click("("))
     button_Par_OPEN.grid(row=9,column=0)
@@ -157,59 +181,36 @@ def Calculator_Gui():
 
     button_power= Button(root, text="x^2",bg="#043738",fg='white',  padx=40, pady=20, command= button_division)
 
-    button_point= Button(root, text=".",bg="#043738",fg='white',  padx=40, pady=20, command= button_division)
+    button_point= Button(root, text=".",bg="#3e3e42",fg='white',  padx=40, pady=20, command= button_division)
 
-    
     # Put the buttons on the screen
 
-    button_1.grid(row=4,column=0)
-    button_2.grid(row=4,column=1)
-    button_3.grid(row=4,column=2)
+    button_1.grid(row=4,column=0, sticky=W+E)
+    button_2.grid(row=4,column=1, sticky=W+E)
+    button_3.grid(row=4,column=2, sticky=W+E)
 
-    button_4.grid(row=3,column=0)
-    button_5.grid(row=3,column=1)
-    button_6.grid(row=3,column=2)
+    button_4.grid(row=3,column=0, sticky=W+E)
+    button_5.grid(row=3,column=1, sticky=W+E)
+    button_6.grid(row=3,column=2, sticky=W+E)
 
-    button_7.grid(row=2,column=0)
-    button_8.grid(row=2,column=1)
-    button_9.grid(row=2,column=2)
+    button_7.grid(row=2,column=0, sticky=W+E)
+    button_8.grid(row=2,column=1, sticky=W+E)
+    button_9.grid(row=2,column=2, sticky=W+E)
 
-    button_0.grid(row=5,column=0)
+    button_0.grid(row=5,column=0, sticky=W+E)
 
-    button_clear.grid(row=5,column=1,columnspan=2)
-    button_add.grid(row=6,column=0)
-    button_equal.grid(row=6,column=1,columnspan=2)
+    button_clear.grid(row=5,column=1,columnspan=2, sticky=W+E)
+    button_equal.grid(row=6,column=1,columnspan=2, sticky=W+E)
+    button_point.grid(row=6,column=0,sticky=W+E)
 
+    button_add.grid(row=8,column=0)
     button_subtract.grid(row=7,column=0)
     button_multiply.grid(row=7,column=1)
     button_divide.grid(row=7,column=2)
 
     button_sqrt.grid(row=8,column=2)
-
     button_power.grid(row=8,column=1)
 
-    button_point.grid(row=8,column=0)
-
-button100 =[0]
-        
-def SciCalculator_Gui():
-    button_100 =  Button(root, text="1", padx=40, pady=20,bg="#3e3e42",fg='white', command=lambda: button_click(1))
-    button_100.grid(row=0,column=40)
-    button100[0]= button_100
-
-
-def change_gui(alla):
-    print("change")
-    SciCalculator_Gui()
-    root.geometry("500x550")
-
-def return_gui(retui):
-    button100[0].destroy()
-
-
-root.bind("b", return_gui)
-
-root.bind("a", change_gui)
 
 Calculator_Gui()
 
